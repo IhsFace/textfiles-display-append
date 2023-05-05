4d93cc5e5ceaeceb1d973c7fbc3042a46867e5c0
IhsFace <ihsaanifzal75@gmail.com>
2023-05-05 20:02:46 +1000

    Update files

---
+++ sac-score-display/sac-score-display/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        btnDisplay = New Button()
+        btnAppend = New Button()
+        btnAverageSacScore = New Button()
+        btnHighestSacScore = New Button()
+        btnLowestSacScore = New Button()
+        lstData = New ListBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(150, 10)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(281, 26)
+        Label1.TabIndex = 0
+        Label1.Text = "Append and Display Scores"
+        ' 
+        ' btnDisplay
+        ' 
+        btnDisplay.Font = New Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point)
+        btnDisplay.Location = New Point(325, 50)
+        btnDisplay.Name = "btnDisplay"
+        btnDisplay.Size = New Size(100, 50)
+        btnDisplay.TabIndex = 1
+        btnDisplay.Text = "Display"
+        btnDisplay.UseVisualStyleBackColor = True
+        ' 
+        ' btnAppend
+        ' 
+        btnAppend.Font = New Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point)
+        btnAppend.Location = New Point(450, 50)
+        btnAppend.Name = "btnAppend"
+        btnAppend.Size = New Size(100, 50)
+        btnAppend.TabIndex = 2
+        btnAppend.Text = "Append"
+        btnAppend.UseVisualStyleBackColor = True
+        ' 
+        ' btnAverageSacScore
+        ' 
+        btnAverageSacScore.Font = New Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point)
+        btnAverageSacScore.Location = New Point(50, 50)
+        btnAverageSacScore.Name = "btnAverageSacScore"
+        btnAverageSacScore.Size = New Size(225, 75)
+        btnAverageSacScore.TabIndex = 3
+        btnAverageSacScore.Text = "Display the average SAC Score"
+        btnAverageSacScore.UseVisualStyleBackColor = True
+        ' 
+        ' btnHighestSacScore
+        ' 
+        btnHighestSacScore.Font = New Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point)
+        btnHighestSacScore.Location = New Point(50, 150)
+        btnHighestSacScore.Name = "btnHighestSacScore"
+        btnHighestSacScore.Size = New Size(225, 75)
+        btnHighestSacScore.TabIndex = 4
+        btnHighestSacScore.Text = "Display the highest SAC Score"
+        btnHighestSacScore.UseVisualStyleBackColor = True
+        ' 
+        ' btnLowestSacScore
+        ' 
+        btnLowestSacScore.Font = New Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point)
+        btnLowestSacScore.Location = New Point(50, 250)
+        btnLowestSacScore.Name = "btnLowestSacScore"
+        btnLowestSacScore.Size = New Size(225, 75)
+        btnLowestSacScore.TabIndex = 5
+        btnLowestSacScore.Text = "Display the lowest SAC Score"
+        btnLowestSacScore.UseVisualStyleBackColor = True
+        ' 
+        ' lstData
+        ' 
+        lstData.FormattingEnabled = True
+        lstData.ItemHeight = 15
+        lstData.Location = New Point(300, 125)
+        lstData.Name = "lstData"
+        lstData.Size = New Size(250, 199)
+        lstData.TabIndex = 6
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(584, 361)
+        Controls.Add(lstData)
+        Controls.Add(btnLowestSacScore)
+        Controls.Add(btnHighestSacScore)
+        Controls.Add(btnAverageSacScore)
+        Controls.Add(btnAppend)
+        Controls.Add(btnDisplay)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "SAC SCORES"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnAppend As Button
+    Friend WithEvents btnAverageSacScore As Button
+    Friend WithEvents btnHighestSacScore As Button
+    Friend WithEvents btnLowestSacScore As Button
+    Friend WithEvents lstData As ListBox
+End Class
---
+++ sac-score-display/sac-score-display/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.IO
+Imports System.Text
+Public Class Form1
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim SacScoresFile As StreamReader = File.OpenText("SacScores.txt")
+        lstData.Items.Clear()
+        Do While SacScoresFile.Peek <> -1
+            lstData.Items.Add(SacScoresFile.ReadLine())
+        Loop
+        SacScoresFile.Close()
+    End Sub
+
+    Private Sub btnAppend_Click(sender As Object, e As EventArgs) Handles btnAppend.Click
+        Dim SacScoresFile As StreamWriter = File.AppendText("SacScores.txt")
+        Dim SACScore As String = InputBox("Please enter a SAC Score between 0 and 100 inclusive.", "Enter Score")
+        If Val(SACScore) >= 0 And Val(SACScore) <= 100 Then
+            SacScoresFile.WriteLine(SACScore)
+            MessageBox.Show("Your score has been appended to the textfile.")
+        Else
+            MessageBox.Show("Your score has not been appended to the textfile. Please enter a SAC Score between 0 and 100 inclusive.")
+        End If
+        SacScoresFile.Close()
+    End Sub
+
+    Private Sub btnAverageSacScore_Click(sender As Object, e As EventArgs) Handles btnAverageSacScore.Click
+        Dim SACScore As Integer
+        Dim TotalScore As Integer
+        Dim AvgSacScore As Double
+        Dim NoOfScores As Integer
+        Dim SacScoresFile As StreamReader = File.OpenText("SacScores.txt")
+        lstData.Items.Clear()
+        Do While SacScoresFile.Peek <> -1
+            SACScore = Val(SacScoresFile.ReadLine())
+            TotalScore = TotalScore + SACScore
+            NoOfScores = NoOfScores + 1
+        Loop
+        AvgSacScore = TotalScore / NoOfScores
+        lstData.Items.Add("Average SAC Score: " & AvgSacScore)
+        SacScoresFile.Close()
+    End Sub
+
+    Private Sub btnHighestSacScore_Click(sender As Object, e As EventArgs) Handles btnHighestSacScore.Click
+        Dim SACScore As Integer
+        Dim HighestSacScore As Integer = Integer.MinValue
+        Dim SacScoresFile As StreamReader = File.OpenText("SacScores.txt")
+        lstData.Items.Clear()
+        Do While SacScoresFile.Peek <> -1
+            SACScore = Val(SacScoresFile.ReadLine())
+            If SACScore > HighestSacScore Then HighestSacScore = SACScore
+        Loop
+        lstData.Items.Add("Highest SAC Score: " & HighestSacScore)
+        SacScoresFile.Close()
+    End Sub
+
+    Private Sub btnLowestSacScore_Click(sender As Object, e As EventArgs) Handles btnLowestSacScore.Click
+        Dim SACScore As Integer
+        Dim LowestSacScore As Integer = Integer.MaxValue
+        Dim SacScoresFile As StreamReader = File.OpenText("SacScores.txt")
+        lstData.Items.Clear()
+        Do While SacScoresFile.Peek <> -1
+            SACScore = Val(SacScoresFile.ReadLine())
+            If SACScore < LowestSacScore Then LowestSacScore = SACScore
+        Loop
+        lstData.Items.Add("Lowest SAC Score: " & LowestSacScore)
+        SacScoresFile.Close()
+    End Sub
+End Class
