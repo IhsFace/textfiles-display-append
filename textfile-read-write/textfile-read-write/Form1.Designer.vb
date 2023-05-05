3e12a5a1d23cb89124bd3db239c49851391d18a7
IhsFace <ihsaanifzal75@gmail.com>
2023-05-05 12:59:11 +1000

    Update files

---
+++ textfile-read-write/textfile-read-write/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblTitle = New Label()
+        btnDisplay = New Button()
+        btnAppend = New Button()
+        btnReadUnknownLengthFile = New Button()
+        lstData = New ListBox()
+        SuspendLayout()
+        ' 
+        ' lblTitle
+        ' 
+        lblTitle.AutoSize = True
+        lblTitle.Font = New Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Point)
+        lblTitle.Location = New Point(45, 30)
+        lblTitle.Name = "lblTitle"
+        lblTitle.Size = New Size(501, 26)
+        lblTitle.TabIndex = 0
+        lblTitle.Text = "A demonstration of reading and writing to a Textfile"
+        ' 
+        ' btnDisplay
+        ' 
+        btnDisplay.Font = New Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point)
+        btnDisplay.Location = New Point(35, 100)
+        btnDisplay.Name = "btnDisplay"
+        btnDisplay.Size = New Size(240, 50)
+        btnDisplay.TabIndex = 1
+        btnDisplay.Text = "Display Contents of textfile"
+        btnDisplay.UseVisualStyleBackColor = True
+        ' 
+        ' btnAppend
+        ' 
+        btnAppend.Font = New Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point)
+        btnAppend.Location = New Point(35, 175)
+        btnAppend.Name = "btnAppend"
+        btnAppend.Size = New Size(240, 50)
+        btnAppend.TabIndex = 2
+        btnAppend.Text = "Append data to textfile"
+        btnAppend.UseVisualStyleBackColor = True
+        ' 
+        ' btnReadUnknownLengthFile
+        ' 
+        btnReadUnknownLengthFile.Font = New Font("Microsoft Sans Serif", 14F, FontStyle.Regular, GraphicsUnit.Point)
+        btnReadUnknownLengthFile.Location = New Point(35, 250)
+        btnReadUnknownLengthFile.Name = "btnReadUnknownLengthFile"
+        btnReadUnknownLengthFile.Size = New Size(240, 65)
+        btnReadUnknownLengthFile.TabIndex = 3
+        btnReadUnknownLengthFile.Text = "Read a file of unknown length"
+        btnReadUnknownLengthFile.UseVisualStyleBackColor = True
+        ' 
+        ' lstData
+        ' 
+        lstData.FormattingEnabled = True
+        lstData.ItemHeight = 15
+        lstData.Location = New Point(330, 100)
+        lstData.Name = "lstData"
+        lstData.Size = New Size(216, 214)
+        lstData.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(584, 361)
+        Controls.Add(lstData)
+        Controls.Add(btnReadUnknownLengthFile)
+        Controls.Add(btnAppend)
+        Controls.Add(btnDisplay)
+        Controls.Add(lblTitle)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnAppend As Button
+    Friend WithEvents btnReadUnknownLengthFile As Button
+    Friend WithEvents lstData As ListBox
+End Class
---
+++ textfile-read-write/textfile-read-write/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.IO
+Imports System.Text
+Public Class Form1
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        Dim TestScores As Integer
+        Dim names As String
+        Dim ExampleFile As StreamReader = File.OpenText("TestScoresData.txt")
+        lstData.Items.Clear()
+        For i = 0 To 4
+            names = ExampleFile.ReadLine()
+            TestScores = ExampleFile.ReadLine()
+            lstData.Items.Add(names & " " & TestScores)
+        Next
+        ExampleFile.Close()
+    End Sub
+
+    Private Sub btnAppend_Click(sender As Object, e As EventArgs) Handles btnAppend.Click
+        Dim ExampleFile As StreamWriter = File.AppendText("TestScoresData.txt")
+        Dim Pname As String = InputBox("Enter the name to add", "Add Name")
+        ExampleFile.WriteLine(Pname)
+        Dim Pscore As Integer = InputBox("Enter the test score to add", "Add score")
+        ExampleFile.WriteLine(Pscore)
+        MessageBox.Show("The name and score has been added to the text file")
+        ExampleFile.Close()
+    End Sub
+
+    Private Sub btnReadUnknownLengthFile_Click(sender As Object, e As EventArgs) Handles btnReadUnknownLengthFile.Click
+        Dim NamesFile As StreamReader = File.OpenText("TestScoresData.txt")
+        lstData.Items.Clear()
+        Dim Pname, Pscore As String
+        Do While NamesFile.Peek <> -1
+            Pname = NamesFile.ReadLine()
+            Pscore = NamesFile.ReadLine()
+            lstData.Items.Add(Pname & " " & Pscore)
+        Loop
+        NamesFile.Close()
+    End Sub
+End Class
